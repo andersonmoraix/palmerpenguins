9bc98f9c1d6f78a223af8200a9e3d4a0c1d8c8b8
Anderson Morais <anderson.mouraissx@gmail.com>
2023-05-19 00:10:01 -0300

    primeiro commit projeto palmer penguins

---
+++ palmer-penguins.R
@@ -0,0 +1,41 @@
+#carregando pacotes
+library("ggplot2")
+library("palmerpenguins")
+
+#criando gráfico com as especies de pinguins
+ggplot(data=penguins) +
+  geom_point(mapping=aes(x = flipper_length_mm, y = body_mass_g))
+
+#grafico que cada especie de pinguin e representada de uma cor diferente
+ggplot(data=penguins) +
+  geom_point(mapping=aes(x = flipper_length_mm, y = body_mass_g, color=species))
+
+#formato grafico diferente para cada tipo de pinguin
+ggplot(data=penguins) +
+  geom_point(mapping=aes(x = flipper_length_mm, y = body_mass_g, shape=species))
+
+#forma e cor grafica de cada espécie diferente
+ggplot(data=penguins) +
+  geom_point(mapping=aes(x = flipper_length_mm, y = body_mass_g, shape=species, color=species))
+
+#alterando tamanho dos objetos graficos
+ggplot(data=penguins) +
+  geom_point(mapping=aes(x = flipper_length_mm, y = body_mass_g, shape=species, color=species, size=species))
+
+#grafico em linha
+ggplot(data=penguins) +
+  geom_smooth(mapping=aes(x=flipper_length_mm, y=body_mass_g))
+
+#grafico com dois elementos gráficos
+ggplot(data=penguins) +
+  geom_smooth(mapping=aes(x=flipper_length_mm, y=body_mass_g)) +
+  geom_point(mapping=aes(x=flipper_length_mm, y=body_mass_g))
+
+#grafico em linha para cada especie
+ggplot(data=penguins) +
+  geom_smooth(mapping=aes(x=flipper_length_mm, y=body_mass_g, linetype=species))
+
+#grafico com dois elementos gráficos geom jitter
+ggplot(data=penguins) +
+  geom_smooth(mapping=aes(x=flipper_length_mm, y=body_mass_g)) +
+  geom_jitter(mapping=aes(x=flipper_length_mm, y=body_mass_g))
